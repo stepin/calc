7052764b10f372c9fdf0ba0858168f7f117ac4f9
Igor Stepin <stepin@stepinsoft.com>
2017-01-14 16:41:22 +0400

    initial layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <calculator></calculator>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Calculator from './components/Calculator'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Calculator
   }
 }
 </script>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="calculator">
+    <div class="line">
+      <button>{{ msg }}</button>
+    </div>
+    <div class="line">
+      <button>{{ msg }}</button>
+    </div>
+    <div class="line">
+      <button>C/CA</button>
+      <button>&larr;</button>
+      <button>rand</button>
+      <button>&divide;</button>
+    </div>
+    <div class="line">
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>&#10005;</button>
+    </div>
+    <div class="line">
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+    </div>
+    <div class="line">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>+</button>
+    </div>
+    <div class="line">
+      <button>,</button>
+      <button>0</button>
+      <button>&plusmn;</button>
+      <button>=</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'calculator',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.calculator {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background: blue;
+}
+
+.line {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background: green;
+}
+
+button {
+  background: gray;
+}
+</style>
