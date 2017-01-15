94422751c54af439f84bc68c789be316ee0879bf
Igor Stepin <stepin@stepinsoft.com>
2017-01-16 00:26:52 +0400

    fixes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 require('normalize.css')
-require('offline-plugin/runtime').install();
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
@@ -12,3 +11,10 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+// Install ServiceWorker and AppCache in the end since
+// it's not most important operation and if main code fails,
+// we do not want it installed
+if (process.env.NODE_ENV === 'production') {
+  require('offline-plugin/runtime').install() // eslint-disable-line global-require
+}
