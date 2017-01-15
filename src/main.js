c54c63415f6ba2490df1c889f7fdca32401305ec
Igor Stepin <stepin@stepinsoft.com>
2017-01-15 23:50:09 +0400

    added offline plugin for AppCache and ServiceWorker

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 require('normalize.css')
+require('offline-plugin/runtime').install();
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
