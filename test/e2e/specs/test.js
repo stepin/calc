54615a37f59bddceb835e9d6e56d21121b2e8220
Igor Stepin <stepin@stepinsoft.com>
2017-01-19 08:11:01 +0400

    first e2e tests works

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -1,7 +1,45 @@
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
 
-function expression(clicks, expressionDisplay, resultDisplay, browser, devServer){
+function pretty (str) {
+  return str.replace(/-/g, '−').replace(/\./g, ',').replace(/\//g, '÷').replace(/\*/g, '×')
+}
+
+function toCssClass (char) {
+  if (char === 'C') {
+    return 'CA'
+  }
+  if (char === 'B') {
+    return 'Backspace'
+  }
+  if (char === 'R') {
+    return 'Randon'
+  }
+  if (char === '/') {
+    return 'Divide'
+  }
+  if (char === '*') {
+    return 'Multiply'
+  }
+  if (char === '-') {
+    return 'Minus'
+  }
+  if (char === '+') {
+    return 'Plus'
+  }
+  if (char === '.') {
+    return 'Period'
+  }
+  if (char === 'S') {
+    return 'Sign'
+  }
+  if (char === '=') {
+    return 'Equal'
+  }
+  return char
+}
+
+function expression (clicks, expressionDisplay, resultDisplay, browser, devServer) {
   expressionDisplay = pretty(expressionDisplay)
   resultDisplay = pretty(resultDisplay)
   browser
@@ -9,12 +47,13 @@ function expression(clicks, expressionDisplay, resultDisplay, browser, devServer
     .waitForElementVisible('#app', 5000)
     .assert.elementPresent('.calculator')
   for (var i = 0, len = clicks.length; i < len; i++) {
-    browser.click('.btn' + clicks[i]);
+    const selector = '.btn' + toCssClass(clicks[i])
+    browser.waitForElementVisible(selector, 100)
+    browser.click(selector);
   }
   browser
     .assert.containsText('.expression', expressionDisplay)
     .assert.containsText('.result', resultDisplay)
-    .end()
 
 }
 
@@ -27,5 +66,6 @@ module.exports = {
 
     expression('', '', '', browser, devServer)
     expression('2+3', '2 + 3', '5', browser, devServer)
+    browser.end()
   }
 }
