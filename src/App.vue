286cac48eaeaa570b6e9043ee98495de5edee6e8
Igor Stepin <stepin@stepinsoft.com>
2017-01-22 18:21:57 +0400

    add-to-homescreen, fastclick & disable selection

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,27 @@
 
 <script>
 import Calculator from './components/Calculator'
+import addToHomescreen from 'add-to-homescreen/src/addtohomescreen.js'
+require('add-to-homescreen/style/addtohomescreen.css')
+
+const _ua = window.navigator.userAgent
+const _isIDevice = (/iphone|ipod|ipad/i).test(_ua)
+const isMobileSafari = _isIDevice && _ua.indexOf('Safari') > -1 && _ua.indexOf('CriOS') < 0
+if (isMobileSafari) {
+  const isStandalone = 'standalone' in window.navigator && window.navigator.standalone
+  if (isStandalone) {
+    // enable fastclick
+    let attachFastClick = require('fastclick')
+    attachFastClick(document.body)
+  } else {
+    // enable add to homescreen tip
+    addToHomescreen({
+      startDelay: 5,
+      skipFirstVisit: true
+
+    })
+  }
+}
 
 export default {
   name: 'app',
@@ -25,4 +46,5 @@ html, body, #app { height: 100%; }
   color: #2c3e50;
 }
 a[href], button { touch-action: manipulation; }
+button { user-select: none; }
 </style>
